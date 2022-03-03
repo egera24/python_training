62c56b1fc430877ec125bc1f5e8d6c95250687ea
egera24 <egera24@gmail.com>
2022-03-03 11:46:36 +0100

    update

---
+++ guess_the_number.py
@@ -0,0 +1,14 @@
+print("Gondolj egy számra!")
+min=1
+max=10
+answer="x"
+while answer != "e":
+    guess=(min+max)//2
+    print("A tippem ", guess)
+    answer=input("k/e/n?")
+    if answer == "k":
+        max=guess-1
+    elif answer == "n"
+        min=guess+1
+print("A gondolt szám ", guess)
+
