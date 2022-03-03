39524043688db4b7dfff018598cf7d8ddcd0544d
egera24 <egera24@gmail.com>
2022-03-03 09:23:38 +0100

    while loop

---
+++ morning_task.py
@@ -0,0 +1,8 @@
+#Van egy listátok, tele számokkal
+# Írjátok ki a számok összegét
+
+sum = 0
+for i in [1,2,3,4,5]:
+    #sum = sum+i
+    sum+=i # a fenti sor helyett ez is használható
+print (sum)
